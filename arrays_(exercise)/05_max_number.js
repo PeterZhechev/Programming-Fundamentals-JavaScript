9d9a2d567dc99443e1e910da38e0dacdcda42be3
Peter Zhechev <petar_vn7@abv.bg>
2023-01-21 00:21:53 +0200

    Update 05_max_number.js

--- arrays_(exercise)/05_max_number.js
+++ arrays_(exercise)/05_max_number.js
@@ -1,30 +1,30 @@
 function maxNumber(arr) {
 
-    let outputArr = [];
-    let firstTempElement = 0;
-    let secondTempElement = 0;
-    let count = 0;
-    let downCount = arr.length;
+    let arrLength = arr.length;
+    let output = [];
+    let isBigger = true;
 
-    for (let i = 0; i < arr.length; i++) {
-        firstTempElement = arr[i];
-        downCount -= 1;
-        for (let j = 0; j < arr.length; j++) {
-            secondTempElement = arr[j];
-
-            if (firstTempElement > secondTempElement) {
-                count++;
-
-                if (count >= arr.length - 1) {
-
-                    outputArr.push(firstTempElement);
-                    count = 0;
-                    break;
-                }
+    for (let i = 0; i < arrLength; i++) {
+        let currentNum = arr.shift();
+        for (let j = 0; j <= arr.length; j++) {
+            let nextNum = arr[j];
+            isBigger = true;
+            if (currentNum <= nextNum) {
+                isBigger = false;
+                break;
             }
         }
+
+        if (isBigger) {
+            output.push(currentNum);
+        }
     }
-    console.log(outputArr.join(' '));
+
+    console.log(output.join(' '));
 }
+
+// maxNumber([14, 24, 3, 19, 15, 17]);
+
+// maxNumber([41, 41, 34, 20]);
 
 maxNumber([27, 19, 42, 2, 13, 45, 48]);
