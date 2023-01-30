b15d6a9a51712af215af721caeb9e95307092d99
Peter Zhechev <petar_vn7@abv.bg>
2023-01-30 19:27:54 +0200

    Update 02_flight_schedule.js

---
+++ objects_and_classes_(more_exercise)/02_flight_schedule.js
@@ -0,0 +1,23 @@
+function flightSchedule(input) {
+
+    
+
+}
+
+flightSchedule(
+    [['WN269 Delaware',
+        'FL2269 Oregon',
+        'WN498 Las Vegas',
+        'WN3145 Ohio',
+        'WN612 Alabama',
+        'WN4010 New York',
+        'WN1173 California',
+        'DL2120 Texas',
+        'KL5744 Illinois',
+        'WN678 Pennsylvania'],
+    ['DL2120 Cancelled',
+        'WN612 Cancelled',
+        'WN1173 Cancelled',
+        'SK430 Cancelled'],
+    ['Cancelled']]
+);
