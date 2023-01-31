82649bed99d7b856dac8a2af7918447ee6afa43f
Peter Zhechev <petar_vn7@abv.bg>
2023-01-31 19:35:28 +0200

    Update 03_school_register.js

--- objects_and_classes_(more_exercise)/03_school_register.js
+++ objects_and_classes_(more_exercise)/03_school_register.js
@@ -2,92 +2,93 @@ function schoolRegister(input) {
 
     let coppyOfInput = input.slice(0);
     let students = [];
-    let studentsInObj = {};
 
-    for (let studentName of coppyOfInput) {
-        [studentName, studentGrade, studentScore] = studentName.split(',');
-        [textToDel, name] = studentName.split(':');
-        [textToDel, grade] = studentGrade.split('Grade: ');
-        grade = Number(grade);
-        grade++;
-        [textToDel, score] = studentScore.split('Graduated with an average score: ');
-        let gradeAndName = grade + ' ' + name.trim();
-        let nameGradeScore = gradeAndName + ' ' + score;
-        students.push(nameGradeScore);
+    for (el of coppyOfInput) {
+        let splitted = el.split('Graduated with an average score: ');
+        let el1 = splitted[0].split('Student name: ');
+        let el2 = Number(splitted[1]);
+        if (el2 >= 3) {
+            students.push(el1[1] + el2)
+        }
     }
 
-    for (el of students) {
-        let splitted = el.split(' ');
-        let grade = Number(splitted.shift());
-        let joinned = splitted.join(' ')
-        studentsInObj[joinned] = grade;
+    let coppyOfStudents = students.slice(0);
+    students = [];
+    for (el of coppyOfStudents) {
+        let splitted = el.split('Grade: ');
+        students.push(splitted[0] + splitted[1]);
     }
 
-    let studentsInObjEntries = Object.entries(studentsInObj);
+    let delCount = 0;
+    while (delCount < students.length) {
 
-    while (studentsInObjEntries.length > 0) {
-        
-        let smaller = Number.MAX_SAFE_INTEGER;
-        for (let el of studentsInObjEntries) {
-            let grade = el[1];
-    
-            if (grade < smaller) {
-                smaller = grade;
+        delCount = 0;
+        let smallest = Number.MAX_SAFE_INTEGER;
+        for (el of students) {
+            [name, grade, score] = el.split(',');
+
+            // take the smallest grade
+
+            if (Number(grade) < smallest) {
+                smallest = Number(grade);
             }
         }
-    
-        let avgScoreForAll = 0;
-        let scoreCounter = 0;
-        let list = [];
-        let currentGrade = 0;
+
+        let counter = 0;
+        let tempList = [];
+        let totalScore = 0;
+        let averageScore = 0;
         let index = 0;
-    
-
-        for (let i = 0; i < studentsInObjEntries.length; i++) {
-            let item = studentsInObjEntries[i];
-            let student = item[0];
-            let grade = item[1];
-            let splitted = student.split(' ');
-            let score = Number(splitted.pop());
-            let studentName = splitted.join(' ');
-            if (score < 3) {
-                studentsInObjEntries.splice(index, 1);
-            }
-
-            if (grade === smaller) {
-                if (score >= 3.00) {
-                    avgScoreForAll += score;
-                    scoreCounter++;
-                    list.push(studentName);
-                    currentGrade = grade;
-                    studentsInObjEntries.splice(index, 1);
-                } 
+        for (el2 of students) {
+            [name2, grade2, score2] = el2.split(',');
+            if (Number(grade2) === smallest) {
+                counter++;
+                tempList.push(name2);
+                totalScore += Number(score2);
+                students.splice(index, 1, 'del');
             }
 
             index++;
         }
-    
-        avgScoreForAll = avgScoreForAll / scoreCounter;
-        console.log(`${currentGrade} Grade`);
-        console.log(`List of students: ${list.join(', ')}`);
-        console.log(`Average annual score from last year: ${avgScoreForAll.toFixed(2)}`);
+
+        averageScore = totalScore / counter;
+
+        console.log(`${Number(smallest) + 1} Grade`);
+        console.log(`List of students: ${tempList.join(', ')}`);
+        console.log(`Average annual score from last year: ${averageScore.toFixed(2)}`);
         console.log('');
+
+        // del count
+        for (el3 of students) {
+            if (el3 === 'del') {
+                delCount++;
+            }
+        }
     }
-
-
 }
 
+// schoolRegister([
+//     "Student name: Mark, Grade: 8, Graduated with an average score: 4.75",
+//     "Student name: Ethan, Grade: 9, Graduated with an average score: 5.66",
+//     "Student name: George, Grade: 8, Graduated with an average score: 2.83",
+//     "Student name: Steven, Grade: 10, Graduated with an average score: 4.20",
+//     "Student name: Joey, Grade: 9, Graduated with an average score: 4.90",
+//     "Student name: Angus, Grade: 11, Graduated with an average score: 2.90",
+//     "Student name: Bob, Grade: 11, Graduated with an average score: 5.15",
+//     "Student name: Daryl, Grade: 8, Graduated with an average score: 5.95",
+//     "Student name: Bill, Grade: 9, Graduated with an average score: 6.00",
+//     "Student name: Philip, Grade: 10, Graduated with an average score: 5.05",
+//     "Student name: Peter, Grade: 11, Graduated with an average score: 4.88",
+//     "Student name: Gavin, Grade: 10, Graduated with an average score: 4.00"
+// ]);
+
+
 schoolRegister([
-    "Student name: Mark, Grade: 8, Graduated with an average score: 4.75",
-    "Student name: Ethan, Grade: 9, Graduated with an average score: 5.66",
-    "Student name: George, Grade: 8, Graduated with an average score: 2.83",
-    "Student name: Steven, Grade: 10, Graduated with an average score: 4.20",
-    "Student name: Joey, Grade: 9, Graduated with an average score: 4.90",
-    "Student name: Angus, Grade: 11, Graduated with an average score: 2.90",
-    "Student name: Bob, Grade: 11, Graduated with an average score: 5.15",
-    "Student name: Daryl, Grade: 8, Graduated with an average score: 5.95",
-    "Student name: Bill, Grade: 9, Graduated with an average score: 6.00",
-    "Student name: Philip, Grade: 10, Graduated with an average score: 5.05",
-    "Student name: Peter, Grade: 11, Graduated with an average score: 4.88",
-    "Student name: Gavin, Grade: 10, Graduated with an average score: 4.00"
+    'Student name: George, Grade: 5, Graduated with an average score: 2.75',
+    'Student name: Alex, Grade: 9, Graduated with an average score: 3.66',
+    'Student name: Peter, Grade: 8, Graduated with an average score: 2.83',
+    'Student name: Boby, Grade: 5, Graduated with an average score: 4.20',
+    'Student name: John, Grade: 9, Graduated with an average score: 2.90',
+    'Student name: Steven, Grade: 2, Graduated with an average score: 4.90',
+    'Student name: Darsy, Grade: 1, Graduated with an average score: 5.15'
 ]);
