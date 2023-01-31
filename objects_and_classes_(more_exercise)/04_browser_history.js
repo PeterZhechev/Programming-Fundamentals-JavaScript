602d162c5d6b3f87c4d726fc5c91dea86e55aa09
Peter Zhechev <petar_vn7@abv.bg>
2023-01-31 21:11:38 +0200

    add new tasks

---
+++ objects_and_classes_(more_exercise)/04_browser_history.js
@@ -0,0 +1,12 @@
+function browserHistory(obj, arr) {
+
+
+}
+
+browserHistory({
+    "Browser Name": "Google Chrome", "Open Tabs": ["Facebook", "YouTube", "Google Translate"],
+    "Recently Closed": ["Yahoo", "Gmail"],
+    "Browser Logs": ["Open YouTube", "Open Yahoo", "Open Google Translate", "Close Yahoo", "Open Gmail", "Close Gmail", "Open Facebook"]
+},
+    ["Close Facebook", "Open StackOverFlow", "Open Google"]
+    );
---
+++ objects_and_classes_(more_exercise)/05_sequences.js
@@ -0,0 +1,15 @@
+function sequences(input) {
+
+    let coppyOfInput = input.slice(0);
+    let inputLength = input.length;
+    // check for duplicate arrays;
+    for (arr of coppyOfInput) {
+        let test = JSON.parse(arr)
+    }
+
+}
+
+sequences(["[-3, -2, -1, 0, 1, 2, 3, 4]",
+    "[10, 1, -17, 0, 2, 13]",
+    "[4, -3, 3, -2, 2, -1, 1, 0]"]
+);
