b5ebebf686dea3e0e9a255e44a0b66089160fa1d
Peter Zhechev <petar_vn7@abv.bg>
2023-02-02 00:22:57 +0200

    Update 04_storage.js

--- associative_arrays_(lab)/04_storage.js
+++ associative_arrays_(lab)/04_storage.js
@@ -2,11 +2,26 @@ function storage(itemsAndTheirQuantity) {
 
     let storedItems = {};
 
-    for (let el of itemsAndTheirQuantity)
+    for (let el of itemsAndTheirQuantity) {
         [item, quantity] = el.split(' ');
-    let isExist = false;
+        let isExist = false;
+
+        for (let key in storedItems) {
+
+            if (key === item) {
+                isExist = true
+            }
+        }
+
+        if (isExist) {
+            storedItems[item] += Number(quantity);
+        } else {
+            storedItems[item] = Number(quantity);
+        }
+    }
+
     for (let key in storedItems) {
-        
+        console.log(`${key} -> ${storedItems[key]}`);
     }
 }
 
