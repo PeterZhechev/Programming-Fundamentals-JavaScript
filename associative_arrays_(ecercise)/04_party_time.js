8f8e8c5efe389b1ae7c4d85f758266083df7e726
Peter Zhechev <petar_vn7@abv.bg>
2023-02-06 19:34:48 +0200

    Update 04_party_time.js

--- associative_arrays_(ecercise)/04_party_time.js
+++ associative_arrays_(ecercise)/04_party_time.js
@@ -27,11 +27,42 @@ function partyTime(input) {
     }
 
     comingGuests.shift();
-    // END of: get coming guests
 
-    let outputGuests = {};
+    let listWithTheGuestsObj = {};
+    let comingGuestsObj = {};
+    
+    for (let i = 0; i < comingGuests.length; i++) {
+        let currentCommingGuest = comingGuests[i];
+        for (let j = 0; j < listWithTheGuests.length; j++) {
+            let currentGuestInList = listWithTheGuests[j];
+            if (currentCommingGuest === currentGuestInList) {
+                listWithTheGuests[j] = 'IN';
+                break;
+            }
+        }
+    }
 
+    let guestsCount = 0;
+    let vipGuests = [];
+    let regularGuests = [];
 
+    for(let el of listWithTheGuests) {
+        let guestType = el[0];
+        if (el !== 'IN') {
+            guestsCount++;
+            if (guestType >= 0) {
+                vipGuests.push(el);
+            } else {
+                regularGuests.push(el);
+            }
+        }
+    }
+
+    let sorted = vipGuests.concat(regularGuests);
+    console.log(guestsCount);
+    for (let el of sorted) {
+        console.log(el);
+    }
 }
 
 partyTime([
@@ -45,3 +76,34 @@ partyTime([
     'Ce8vwPmE',
     'SVQXQCbc'
 ]);
+
+// partyTime(['m8rfQBvl',
+// 'fc1oZCE0',
+// 'UgffRkOn',
+// '7ugX7bm0',
+// '9CQBGUeJ',
+// '2FQZT3uC',
+// 'dziNz78I',
+// 'mdSGyQCJ',
+// 'LjcVpmDL',
+// 'fPXNHpm1',
+// 'HTTbwRmM',
+// 'B5yTkMQi',
+// '8N0FThqG',
+// 'xys2FYzn',
+// 'MDzcM9ZK',
+// 'PARTY',
+// '2FQZT3uC',
+// 'dziNz78I',
+// 'mdSGyQCJ',
+// 'LjcVpmDL',
+// 'fPXNHpm1',
+// 'HTTbwRmM',
+// 'B5yTkMQi',
+// '8N0FThqG',
+// 'm8rfQBvl',
+// 'fc1oZCE0',
+// 'UgffRkOn',
+// '7ugX7bm0',
+// '9CQBGUeJ'
+// ]);
