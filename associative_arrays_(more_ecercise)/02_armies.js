9ae419cbe01fd0ae1b26b7e933d9cb10dac3b949
Peter Zhechev <petar_vn7@abv.bg>
2023-03-03 20:08:16 +0200

    Update 02_armies.js

--- associative_arrays_(more_ecercise)/02_armies.js
+++ associative_arrays_(more_ecercise)/02_armies.js
@@ -117,22 +117,6 @@ function armies(input) {
     }
 }
 
-// armies([
-//     'Rick Burr arrives',
-//     'Rick Burr defeated',
-//     'Fergus: Wexamp, 30245',
-//     'Rick Burr: Juard, 50000',
-//     'Findlay arrives',
-//     'Findlay: Wexamp, 34540',
-//     'Wexamp + 6000',
-//     'Juard + 1350',
-//     'Britox + 4500',
-//     'Porter arrives',
-//     'Porter: Legion, 55000',
-//     'Legion + 302',
-//     'Porter: Retix, 3205',
-// ]);
-
 armies(['Rick Burr arrives', 'Fergus: Wexamp, 30245', 'Rick Burr: Juard, 50000', 'Findlay arrives', 'Findlay: Britox, 34540', 'Wexamp + 6000', 'Juard + 1350', 'Britox + 4500', 'Porter arrives', 'Porter: Legion, 55000', 'Legion + 302', 'Rick Burr defeated', 'Porter: Retix, 3205']);
 
 // armies(['Rick Burr arrives', 'Findlay arrives', 'Rick Burr: Juard, 1500', 'Wexamp arrives', 'Findlay: Wexamp, 34540', 'Wexamp + 340', 'Wexamp: Britox, 1155', 'Wexamp: Juard, 43423'])
