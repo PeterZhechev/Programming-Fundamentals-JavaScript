b96492255c8d49e2b8693bac56c06cb39edc1a92
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-03-04 04:05:41 +0200

    Create 03_comments.js

---
+++ associative_arrays_(more_ecercise)/03_comments.js
@@ -0,0 +1,18 @@
+function comments(params) {
+
+}
+
+comments([
+    'user aUser123',
+    'someUser posts on someArticle: NoTitle, stupidComment',
+    'article Books',
+    'article Movies',
+    'article Shopping',
+    'user someUser',
+    'user uSeR4',
+    'user lastUser',
+    'uSeR4 posts on Books: I like books, I do really like them',
+    'uSeR4 posts on Movies: I also like movies, I really do',
+    'someUser posts on Shopping: title, I go shopping every day',
+    'someUser posts on Movies: Like, I also like movies very much',
+]);
