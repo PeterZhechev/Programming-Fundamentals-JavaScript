b166b0bd9a8ceed22e21132eb336be48f9829a03
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-03-05 20:04:26 +0200

    Update 04_book_shelf.js

--- associative_arrays_(more_ecercise)/04_book_shelf.js
+++ associative_arrays_(more_ecercise)/04_book_shelf.js
@@ -77,19 +77,29 @@ function bookShelf(inputData) {
 }
 
 
+// bookShelf([
+//     '1 -> history',
+//     '1 -> action',
+//     'Death in Time: Criss Bell, mystery',
+//     '2 -> mystery',
+//     '3 -> sci-fi',
+//     'Child of Silver: Bruce Rich, mystery',
+//     'Hurting Secrets: Dustin Bolt, action',
+//     'Future of Dawn: Aiden Rose, sci-fi',
+//     'Lions and Rats: Gabe Roads, history',
+//     '2 -> romance',
+//     'Effect of the Void: Shay B, romance',
+//     'Losing Dreams: Gail Starr, sci-fi',
+//     'Name of Earth: Jo Bell, sci-fi',
+//     'Pilots of Stone: Brook Jay, history'
+// ]);
+
 bookShelf([
-    '1 -> history',
-    '1 -> action',
-    'Death in Time: Criss Bell, mystery',
-    '2 -> mystery',
-    '3 -> sci-fi',
+    '1 -> mystery',
+    '2 -> sci-fi',
     'Child of Silver: Bruce Rich, mystery',
-    'Hurting Secrets: Dustin Bolt, action',
-    'Future of Dawn: Aiden Rose, sci-fi',
     'Lions and Rats: Gabe Roads, history',
-    '2 -> romance',
     'Effect of the Void: Shay B, romance',
     'Losing Dreams: Gail Starr, sci-fi',
-    'Name of Earth: Jo Bell, sci-fi',
-    'Pilots of Stone: Brook Jay, history'
+    'Name of Earth: Jo Bell, sci-fi'
 ]);
