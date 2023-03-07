0149d7a01bc701c312aa2c8a5d0e7d7bf1be8b44
Peter Zhechev <petar_vn7@abv.bg>
2023-03-07 23:28:55 +0200

    Update 05_soft_uni_students.js

--- associative_arrays_(more_ecercise)/05_soft_uni_students.js
+++ associative_arrays_(more_ecercise)/05_soft_uni_students.js
@@ -1,73 +1,142 @@
-function softUniStudents(input) {
+function softUniStudents(inputData) {
 
     const courses = {};
-    const coursesCapacity = {};
-    const studentsAndCredits = {};
+    const infoForStudents = {};
+    const registeredStudentsInAvailableCourses = {};
 
-    for (let el of input) {
-        let [courseName, courseCapacity] = el.split(': ');
-        courseCapacity = Number(courseCapacity);
+    for (let el of inputData) {
+        let splittedEl = el.split(' ');
 
-        if (courseCapacity >= 0) {
-            if (!courses.hasOwnProperty(courseName)) {
-                courses[courseName] = {};
-                coursesCapacity[courseName] = courseCapacity;
-            } else {
-                coursesCapacity[courseName] += courseCapacity;
+        if (splittedEl.length === 2) {
+            let courseNameToAdd = splittedEl[0].split('');
+            let removedElement = courseNameToAdd.pop();
+            courseNameToAdd = courseNameToAdd.join('');
+            let courseCapacityToAdd = Number(splittedEl[1]);
+            if (!courses.hasOwnProperty(courseNameToAdd)) {
+                courses[courseNameToAdd] = courseCapacityToAdd;
+            } else if (courses.hasOwnProperty(courseNameToAdd)) {
+                courses[courseNameToAdd] += courseCapacityToAdd;
             }
-        } else {
-            let [usernameAndCredits, eMailAndJoinedCourse] = el.split(' with email ');
-            let [eMail, joinedCourse] = eMailAndJoinedCourse.split(' joins ');
-            let splittedUsernameAndCredits = usernameAndCredits.split('[');
-            let username = splittedUsernameAndCredits[0];
-            let creditsRaw = splittedUsernameAndCredits[1].split(']');
-            let credits = Number(creditsRaw[0]);
-            studentsAndCredits[username] = credits;
 
-            if (courses.hasOwnProperty(joinedCourse)) {
-                if (coursesCapacity[joinedCourse] >= 1) {
-                    coursesCapacity[joinedCourse] -= 1;
-                    courses[joinedCourse][username] = eMail;
+        } else if (splittedEl.length > 2) {
+            let userNameWithCredits = splittedEl[0];
+            let email = splittedEl[3];
+            let course = splittedEl[5];
+            let reversedNum = '';
+            let reversedUsername = '';
+            let isEndOfNumInfo = false;
+            let counter = 0;
+
+            for (let i = userNameWithCredits.length - 2; i >= 0; i--) {
+                let currentChar = userNameWithCredits[i];
+
+                if (currentChar === '[') {
+                    isEndOfNumInfo = true;
+                }
+
+                if (isEndOfNumInfo === false) {
+                    reversedNum += currentChar;
+                } else {
+                    counter++;
+                    if (counter > 1) {
+                        reversedUsername += currentChar;
+                    }
+                }
+            }
+
+            let userName = reversedUsername
+                .split('')
+                .reverse()
+                .join('');
+
+            let credits = reversedNum
+                .split('')
+                .reverse()
+                .join('');
+
+            infoForStudents[userName] = {};
+            infoForStudents[userName]['credits'] = Number(credits);
+            infoForStudents[userName]['email'] = email;
+
+            if (courses.hasOwnProperty(course)) {
+                if (courses[course] > 0) {
+                    courses[course] -= 1;
+                }
+
+                if (registeredStudentsInAvailableCourses.hasOwnProperty(course)) {
+                    registeredStudentsInAvailableCourses[course][userName] = userName;
+                } else if (!registeredStudentsInAvailableCourses.hasOwnProperty(course)) {
+                    registeredStudentsInAvailableCourses[course] = {};
+                    registeredStudentsInAvailableCourses[course][userName] = userName;
                 }
             }
         }
+
     }
 
-    const countOfStudents = {};
+    function calculateCountOfStudentsInCourses(infoAboutRegisteredStudents) {
 
-    for (const key in courses) {
-        const currentCourse = courses[key];
-        countOfStudents[key] = 0;
-        for (const student in currentCourse) {
-            countOfStudents[key] += 1;
+        const studentsCount = {};
+
+        for (const course in infoAboutRegisteredStudents) {
+            let currentCourse = infoAboutRegisteredStudents[course];
+            let counter = 0;
+            for (const student in currentCourse) {
+                counter++;
+            }
+
+            studentsCount[course] = counter;
         }
+
+        return studentsCount;
     }
+
+    let countedStudents = calculateCountOfStudentsInCourses(registeredStudentsInAvailableCourses)
 
     let sortedCountOfStudents = Object
-        .entries(countOfStudents)
+        .entries(countedStudents)
         .sort((a, b) => b[1] - a[1]);
 
-    let sortedStudentsAndCredits = Object
-        .entries(studentsAndCredits)
-        .sort((a, b) => b[1] - a[1]);
-
-    for (const course of sortedCountOfStudents) {
-        console.log(`${course[0]}: ${coursesCapacity[course[0]]} places left`);
-        let currentCourse = courses[course[0]];
-
-        let currentStudents = [];
-
-        for (let el of sortedStudentsAndCredits) {
-            let currentStudent = el[0];
-            if (currentCourse.hasOwnProperty(currentStudent)) {
-                currentStudents.push(el);
-            }
-        }
-
-        for (let student of currentStudents) {
-            console.log(`--- ${student[1]}: ${student[0]}, ${courses[course[0]][student[0]]}`);
+    for (const key in registeredStudentsInAvailableCourses) {
+        let currentCourse = registeredStudentsInAvailableCourses[key];
+        for (const user in currentCourse) {
+            currentCourse[user] = infoForStudents[user];
         }
     }
+
+    function printTheColectedInfo(colectedCourses, colectedInfoForStudents, colectedRegisteredStudentsInAvailableCourses, colectedSortedCountOfStudents) {
+
+        for (let course of colectedSortedCountOfStudents) {
+            const [currentCourse, studentsCount] = course;
+            console.log(`${currentCourse}: ${colectedCourses[currentCourse]} places left`);
+
+            let currentUsers = colectedRegisteredStudentsInAvailableCourses[currentCourse];
+            let currentUsersAndTheirCredits = {};
+            for (let key in currentUsers) {
+                let currentUser = currentUsers[key];
+                currentUsersAndTheirCredits[key] = currentUser.credits;
+            }
+
+            let sortedUsersByCredits = Object
+                .entries(currentUsersAndTheirCredits)
+                .sort((a, b) => {
+                    let sorted = b[1] - a[1];
+
+                    if (sorted === 0) {
+                        sorted = a[0].localeCompare(b[0]);
+                    }
+
+                    return sorted;
+                });
+
+            for (const [key, value] of sortedUsersByCredits) {
+                
+            }
+        }
+    }
+
+    printTheColectedInfo(courses, infoForStudents, registeredStudentsInAvailableCourses, sortedCountOfStudents);
+
 }
 
 // SoftUniStudents(['JavaBasics: 2', 'user1[25] with email user1@user.com joins C#Basics', 'C#Advanced: 3', 'JSCore: 4', 'user2[30] with email user2@user.com joins C#Basics', 'user13[50] with email user13@user.com joins JSCore', 'user1[25] with email user1@user.com joins JSCore', 'user8[18] with email user8@user.com joins C#Advanced', 'user6[85] with email user6@user.com joins JSCore', 'JSCore: 2', 'user11[3] with email user11@user.com joins JavaBasics', 'user45[105] with email user45@user.com joins JSCore', 'user007[20] with email user007@user.com joins JSCore', 'user700[29] with email user700@user.com joins JSCore', 'user900[88] with email user900@user.com joins JSCore']);
