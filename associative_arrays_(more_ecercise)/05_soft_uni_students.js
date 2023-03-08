05ddf6ceaf854ea037d6fa1103e222e6c96ccd09
Peter Zhechev <petar_vn7@abv.bg>
2023-03-08 14:06:02 +0200

    Update 05_soft_uni_students.js

--- associative_arrays_(more_ecercise)/05_soft_uni_students.js
+++ associative_arrays_(more_ecercise)/05_soft_uni_students.js
@@ -47,14 +47,38 @@ function softUniStudents(inputData) {
 
     const formatedInputData = formatInputData(inputData)
 
+    const courseNameAndCapacity = {};
+    const registeredStudents = {};
+    const studentsCountInCourses = {};
+
     for (let formatedLine of formatedInputData) {
 
         if (formatedLine.length === 2) {
-            console.log(formatedLine);
+            const courseName = formatedLine[0];
+            const capacity = formatedLine[1];
+
+            if (!courseNameAndCapacity.hasOwnProperty(courseName)) {
+                courseNameAndCapacity[courseName] = capacity;
+                registeredStudents[courseName] = [];
+                studentsCountInCourses[courseName] = 0;
+            } else {
+                courseNameAndCapacity[courseName] += capacity;
+            }
         } else {
-            console.log(formatedLine);
+            const formatedUsername = formatedLine[0];
+            const userCredits = formatedLine[1];
+            const UserEmail = formatedLine[2];
+            const joinedCourse = formatedLine[3];
+
+            if (registeredStudents.hasOwnProperty(joinedCourse)) {
+                registeredStudents[joinedCourse].push(formatedLine);
+                studentsCountInCourses[joinedCourse]++;
+                courseNameAndCapacity[joinedCourse]--;
+            }
         }
     }
+
+    console.log();
 }
 
 // softUniStudents(['JavaBasics: 2', 'user1[25] with email user1@user.com joins C#Basics', 'C#Advanced: 3', 'JSCore: 4', 'user2[30] with email user2@user.com joins C#Basics', 'user13[50] with email user13@user.com joins JSCore', 'user1[25] with email user1@user.com joins JSCore', 'user8[18] with email user8@user.com joins C#Advanced', 'user6[85] with email user6@user.com joins JSCore', 'JSCore: 2', 'user11[3] with email user11@user.com joins JavaBasics', 'user45[105] with email user45@user.com joins JSCore', 'user007[20] with email user007@user.com joins JSCore', 'user700[29] with email user700@user.com joins JSCore', 'user900[88] with email user900@user.com joins JSCore']);
