64b9d3571bf27f54aace525a07ec5a37ef7370f6
Peter Zhechev <petar_vn7@abv.bg>
2023-03-08 13:43:45 +0200

    Update 05_soft_uni_students.js

--- associative_arrays_(more_ecercise)/05_soft_uni_students.js
+++ associative_arrays_(more_ecercise)/05_soft_uni_students.js
@@ -1,145 +1,64 @@
 function softUniStudents(inputData) {
 
-    const courses = {};
-    const infoForStudents = {};
-    const registeredStudentsInAvailableCourses = {};
+    // const courses = {};
+    // const coursesCapacity = {};
+    // const infoForStudents = {};
 
-    for (let el of inputData) {
-        let splittedEl = el.split(' ');
 
-        if (splittedEl.length === 2) {
-            let courseNameToAdd = splittedEl[0].split('');
-            let removedElement = courseNameToAdd.pop();
-            courseNameToAdd = courseNameToAdd.join('');
-            let courseCapacityToAdd = Number(splittedEl[1]);
-            if (!courses.hasOwnProperty(courseNameToAdd)) {
-                courses[courseNameToAdd] = courseCapacityToAdd;
-            } else if (courses.hasOwnProperty(courseNameToAdd)) {
-                courses[courseNameToAdd] += courseCapacityToAdd;
-            }
+    function formatInputData(rawInputData) {
 
-        } else if (splittedEl.length > 2) {
-            let userNameWithCredits = splittedEl[0];
-            let email = splittedEl[3];
-            let course = splittedEl[5];
-            let reversedNum = '';
-            let reversedUsername = '';
-            let isEndOfNumInfo = false;
-            let counter = 0;
+        const formatedInputDataArr = [];
 
-            for (let i = userNameWithCredits.length - 2; i >= 0; i--) {
-                let currentChar = userNameWithCredits[i];
+        for (const line of rawInputData) {
+            const splittedLine = line.split(' ');
 
-                if (currentChar === '[') {
-                    isEndOfNumInfo = true;
-                }
+            if (splittedLine.length === 2) {
+                let courseNameAndCapacity = [];
+                let rawCourseName = splittedLine[0];
+                let rawCourseNameToArr = rawCourseName.split('');
+                rawCourseNameToArr.pop();
 
-                if (isEndOfNumInfo === false) {
-                    reversedNum += currentChar;
-                } else {
-                    counter++;
-                    if (counter > 1) {
-                        reversedUsername += currentChar;
-                    }
-                }
-            }
+                // final format of course name and capacity
+                const courseName = rawCourseNameToArr.join('');
+                const capacity = Number(splittedLine[1]);
 
-            let userName = reversedUsername
-                .split('')
-                .reverse()
-                .join('');
+                courseNameAndCapacity.push(courseName, capacity);
+                formatedInputDataArr.push(courseNameAndCapacity);
+            } else {
+                let usernameInfo = [];
+                let rawUsername = splittedLine[0];
+                let rawUsernameToArr = rawUsername.split('');
+                rawUsernameToArr.pop();
+                let rawUsernameToStr = rawUsernameToArr.join('');
+                let formatedUsernameToArr = rawUsernameToStr.split('[');
 
-            let credits = reversedNum
-                .split('')
-                .reverse()
-                .join('');
+                const formatedUsername = formatedUsernameToArr[0];
+                const userCredits = Number(formatedUsernameToArr[1]);
+                const UserEmail = splittedLine[3];
+                const joinedCourse = splittedLine[5];
 
-            infoForStudents[userName] = {};
-            infoForStudents[userName]['credits'] = Number(credits);
-            infoForStudents[userName]['email'] = email;
-
-            if (courses.hasOwnProperty(course)) {
-                if (courses[course] > 0) {
-                    courses[course] -= 1;
-                }
-
-                if (registeredStudentsInAvailableCourses.hasOwnProperty(course)) {
-                    registeredStudentsInAvailableCourses[course][userName] = userName;
-                } else if (!registeredStudentsInAvailableCourses.hasOwnProperty(course)) {
-                    registeredStudentsInAvailableCourses[course] = {};
-                    registeredStudentsInAvailableCourses[course][userName] = userName;
-                }
+                usernameInfo.push(formatedUsername, userCredits, UserEmail, joinedCourse);
+                formatedInputDataArr.push(usernameInfo);
             }
         }
 
+        return formatedInputDataArr;
     }
 
-    function calculateCountOfStudentsInCourses(infoAboutRegisteredStudents) {
+    const formatedInputData = formatInputData(inputData)
 
-        const studentsCount = {};
+    for (let formatedLine of formatedInputData) {
 
-        for (const course in infoAboutRegisteredStudents) {
-            let currentCourse = infoAboutRegisteredStudents[course];
-            let counter = 0;
-            for (const student in currentCourse) {
-                counter++;
-            }
-
-            studentsCount[course] = counter;
-        }
-
-        return studentsCount;
-    }
-
-    let countedStudents = calculateCountOfStudentsInCourses(registeredStudentsInAvailableCourses)
-
-    let sortedCountOfStudents = Object
-        .entries(countedStudents)
-        .sort((a, b) => b[1] - a[1]);
-
-    for (const key in registeredStudentsInAvailableCourses) {
-        let currentCourse = registeredStudentsInAvailableCourses[key];
-        for (const user in currentCourse) {
-            currentCourse[user] = infoForStudents[user];
+        if (formatedLine.length === 2) {
+            console.log(formatedLine);
+        } else {
+            console.log(formatedLine);
         }
     }
-
-    function printTheColectedInfo(colectedCourses, colectedInfoForStudents, colectedRegisteredStudentsInAvailableCourses, colectedSortedCountOfStudents) {
-
-        for (let course of colectedSortedCountOfStudents) {
-            const [currentCourse, studentsCount] = course;
-            console.log(`${currentCourse}: ${colectedCourses[currentCourse]} places left`);
-
-            let currentUsers = colectedRegisteredStudentsInAvailableCourses[currentCourse];
-            let currentUsersAndTheirCredits = {};
-            for (let key in currentUsers) {
-                let currentUser = currentUsers[key];
-                currentUsersAndTheirCredits[key] = currentUser.credits;
-            }
-
-            let sortedUsersByCredits = Object
-                .entries(currentUsersAndTheirCredits)
-                .sort((a, b) => {
-                    let sorted = b[1] - a[1];
-
-                    if (sorted === 0) {
-                        sorted = a[0].localeCompare(b[0]);
-                    }
-
-                    return sorted;
-                });
-
-            for (const [key, value] of sortedUsersByCredits) {
-                
-            }
-        }
-    }
-
-    printTheColectedInfo(courses, infoForStudents, registeredStudentsInAvailableCourses, sortedCountOfStudents);
-
 }
 
-// SoftUniStudents(['JavaBasics: 2', 'user1[25] with email user1@user.com joins C#Basics', 'C#Advanced: 3', 'JSCore: 4', 'user2[30] with email user2@user.com joins C#Basics', 'user13[50] with email user13@user.com joins JSCore', 'user1[25] with email user1@user.com joins JSCore', 'user8[18] with email user8@user.com joins C#Advanced', 'user6[85] with email user6@user.com joins JSCore', 'JSCore: 2', 'user11[3] with email user11@user.com joins JavaBasics', 'user45[105] with email user45@user.com joins JSCore', 'user007[20] with email user007@user.com joins JSCore', 'user700[29] with email user700@user.com joins JSCore', 'user900[88] with email user900@user.com joins JSCore']);
+// softUniStudents(['JavaBasics: 2', 'user1[25] with email user1@user.com joins C#Basics', 'C#Advanced: 3', 'JSCore: 4', 'user2[30] with email user2@user.com joins C#Basics', 'user13[50] with email user13@user.com joins JSCore', 'user1[25] with email user1@user.com joins JSCore', 'user8[18] with email user8@user.com joins C#Advanced', 'user6[85] with email user6@user.com joins JSCore', 'JSCore: 2', 'user11[3] with email user11@user.com joins JavaBasics', 'user45[105] with email user45@user.com joins JSCore', 'user007[20] with email user007@user.com joins JSCore', 'user700[29] with email user700@user.com joins JSCore', 'user900[88] with email user900@user.com joins JSCore']);
+
 
 softUniStudents([
     'JavaBasics: 15',
@@ -154,3 +73,4 @@ softUniStudents([
     'JSCore: 8',
     'user23[62] with email user23@user.com joins JSCore'
 ]);
+
