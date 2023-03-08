179fc83a08e338997db6620bc22d8f198cd7d9c0
Peter Zhechev <petar_vn7@abv.bg>
2023-03-08 14:22:44 +0200

    Update 05_soft_uni_students.js

--- associative_arrays_(more_ecercise)/05_soft_uni_students.js
+++ associative_arrays_(more_ecercise)/05_soft_uni_students.js
@@ -1,10 +1,5 @@
 function softUniStudents(inputData) {
 
-    // const courses = {};
-    // const coursesCapacity = {};
-    // const infoForStudents = {};
-
-
     function formatInputData(rawInputData) {
 
         const formatedInputDataArr = [];
@@ -65,20 +60,35 @@ function softUniStudents(inputData) {
                 courseNameAndCapacity[courseName] += capacity;
             }
         } else {
-            const formatedUsername = formatedLine[0];
-            const userCredits = formatedLine[1];
-            const UserEmail = formatedLine[2];
+            // const formatedUsername = formatedLine[0];
+            // const userCredits = formatedLine[1];
+            // const UserEmail = formatedLine[2];
             const joinedCourse = formatedLine[3];
 
             if (registeredStudents.hasOwnProperty(joinedCourse)) {
-                registeredStudents[joinedCourse].push(formatedLine);
-                studentsCountInCourses[joinedCourse]++;
-                courseNameAndCapacity[joinedCourse]--;
+                if (courseNameAndCapacity[joinedCourse] > 0) {
+                    registeredStudents[joinedCourse].push(formatedLine);
+                    studentsCountInCourses[joinedCourse]++;
+                    courseNameAndCapacity[joinedCourse]--;
+                }
             }
         }
     }
 
-    console.log();
+    let sortedStudentsCountInCourses = Object
+        .entries(studentsCountInCourses)
+        .sort((a, b) => b[1] - a[1]);
+
+    for (let course of sortedStudentsCountInCourses) {
+        console.log(`${course[0]}: ${courseNameAndCapacity[course[0]]} places left`);
+
+        let currentRegisteredStudents = registeredStudents[course[0]];
+        let sortedRegisteredStudents = currentRegisteredStudents.sort((a, b) => b[1] - a[1]);
+
+        for (const student of sortedRegisteredStudents) {
+            console.log(`--- ${student[1]}: ${student[0]}, ${student[2]}`);
+        }
+    }
 }
 
 // softUniStudents(['JavaBasics: 2', 'user1[25] with email user1@user.com joins C#Basics', 'C#Advanced: 3', 'JSCore: 4', 'user2[30] with email user2@user.com joins C#Basics', 'user13[50] with email user13@user.com joins JSCore', 'user1[25] with email user1@user.com joins JSCore', 'user8[18] with email user8@user.com joins C#Advanced', 'user6[85] with email user6@user.com joins JSCore', 'JSCore: 2', 'user11[3] with email user11@user.com joins JavaBasics', 'user45[105] with email user45@user.com joins JSCore', 'user007[20] with email user007@user.com joins JSCore', 'user700[29] with email user700@user.com joins JSCore', 'user900[88] with email user900@user.com joins JSCore']);
