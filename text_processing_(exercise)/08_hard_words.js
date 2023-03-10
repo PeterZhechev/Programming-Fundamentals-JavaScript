0393f525d02c42c71c70e5b5def5f60ca5fc6554
Peter Zhechev <petar_vn7@abv.bg>
2023-03-10 14:07:49 +0200

    Create 08_hard_words.js

---
+++ text_processing_(exercise)/08_hard_words.js
@@ -0,0 +1,5 @@
+function hardWords(str, arr) {
+
+}
+
+console.log(hardWords(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.', ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']]));
