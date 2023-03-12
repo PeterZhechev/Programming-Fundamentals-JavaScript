42c6cb6234138985e9e9aad7f1d59c76da1a6fdc
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-03-12 13:36:50 +0200

    Create 06_melrah_shake.js

---
+++ text_processing_(more_exercise)/06_melrah_shake.js
@@ -0,0 +1,45 @@
+function melrahShake(arr) {
+
+    let str = arr[0];
+    let pattern = arr[1];
+    let splittedStr = str.split('');
+    let indices = [];
+    let isExist = false;
+
+    while (pattern.length > 0) {
+
+        for (let i = 0; i < splittedStr.length; i++) {
+            let currentPart = splittedStr.slice(i, i + pattern.length);
+            let joinned = currentPart.join('');
+            if (joinned === pattern) {
+                indices.push(i);
+            }
+        }
+
+        if (indices.length >= 2) {
+            let firstPart = indices.shift();
+            let lastPart = indices.pop();
+            splittedStr.splice(firstPart, pattern.length);
+            splittedStr.splice(lastPart - pattern.length, pattern.length);
+            console.log('Shaked it.');
+            let harlfOfThePattern = pattern.length / 2;
+            let splittedPattern = pattern.split('');
+            splittedPattern.splice(harlfOfThePattern, 1);
+            pattern = splittedPattern.join('');
+            isExist = true;
+        } else {
+            isExist = false;
+        }
+
+        if (!isExist) {
+            console.log('No shake.');
+            break;
+        }
+    }
+    
+    console.log(splittedStr.join(''));
+}
+
+melrahShake(['##mtm!!mm.mm*mtm.#', 'mtm']);
+
+// melrahShake(['astalavista baby', 'sta']);
