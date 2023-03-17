cc6fe132ff819b014a6cc0603fa92759b518488e
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-03-17 16:39:16 +0200

    Create 02_race.js

---
+++ regular_expressions_(exercise)/02_race.js
@@ -0,0 +1,16 @@
+function race(input) {
+
+    let participants = input.shift().split(', ');
+    console.log();
+}
+
+race([
+    'George, Peter, Bill, Tom',
+    'G4e@55or%6g6!68e!!@ ',
+    'R1@!3a$y4456@',
+    'B5@i@#123ll',
+    'G@e54o$r6ge#',
+    '7P%et^#e5346r',
+    'T$o553m&6',
+    'end of race'
+]);
