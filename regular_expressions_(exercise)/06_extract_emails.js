7b9f7587e79d82bef508b04b098247aa404d92b4
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-03-18 14:31:50 +0200

    Update 06_extract_emails.js

--- regular_expressions_(exercise)/06_extract_emails.js
+++ regular_expressions_(exercise)/06_extract_emails.js
@@ -1,8 +1,19 @@
 function extractEmails(inputText) {
-    
-    let pattern = /^(?<user>[A-Za-z0-9]+[.\-_]*[A-Za-z0-9]+)@(?<host>[A-Za-z\-]+[.][A-Za-z\-]+[.A-Za-z\-]*)$\b/;
+
+    let pattern = /(?<valid>^[A-Za-z0-9]+[.\-_]*[A-Za-z0-9]+@[A-Za-z\-]+[.][A-Za-z\-]+[.A-Za-z\-]*)\b/g;
+
+    let textArr = inputText.split(' ');
+
+    for (let currentText of textArr) {
+
+        let match = currentText.matchAll(pattern);
+        
+        for (let matched of match) {
+            console.log(matched.groups.valid);
+        }
+    }
 }
 
-extractEmails('Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.');
+extractEmails('Many users @ SoftUni confuse email addresses. We @ Softuni.BG provide high-quality training @ home or @ class. –- steve.parker@softuni.de.');
 
 
