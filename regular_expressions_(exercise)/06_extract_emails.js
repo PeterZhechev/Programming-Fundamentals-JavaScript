45619b0c1c779211220ff784caa554f64ec63eeb
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-03-18 13:17:58 +0200

    Update 06_extract_emails.js

--- regular_expressions_(exercise)/06_extract_emails.js
+++ regular_expressions_(exercise)/06_extract_emails.js
@@ -1,6 +1,6 @@
 function extractEmails(inputText) {
     
-    let pattern = /(?<username>^[A-Za-z0-9]+[.\-_]*[A-Za-z0-9]+$)/g;
+    let pattern = /^(?<user>[A-Za-z0-9]+[.\-_]*[A-Za-z0-9]+)@(?<host>[A-Za-z\-]+[.][A-Za-z\-]+[.A-Za-z\-]*)$\b/;
 }
 
 extractEmails('Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.');
