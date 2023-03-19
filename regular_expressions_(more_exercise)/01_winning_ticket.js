0bae17799d7051f2aa8bb24a0e565dbf2771fdbb
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-03-19 19:33:09 +0200

    Update 01_winning_ticket.js

--- regular_expressions_(more_exercise)/01_winning_ticket.js
+++ regular_expressions_(more_exercise)/01_winning_ticket.js
@@ -1,18 +1,21 @@
 function winningTicket(input) {
-    
+
     let winningTicketLength = 20;
 
     let pattern6to9 = /.+([@#$^]{6,}).+([@#$^]{6,}).+/;
-    let jackpotPattern = /([@#$^]{6,10})([@#$^]{6,10})/;
+    // let jackpotPattern = /^(?<group1>([@#$^])\2{9})(?<group2>([@#$^])\4{9})$/;
+    let jackpotPattern = /^(?<group1>([@#$^])\2{9})(?<group2>\2{10})$/;
 
     let splitted = input.split(', ');
     let matchJackpot = [];
 
     for (let el of splitted) {
+
         matchJackpot = el.match(jackpotPattern);
+
         if (matchJackpot) {
-            if (match[0].length === 20) {
-                console.log(`ticket "${matchJackpot[0]}" - ${matchJackpot[1].length}${matchJackpot[0][0]} Jackpot`);
+            if (matchJackpot[0].length === 20) {
+                console.log(`ticket "${matchJackpot[0]}" - ${matchJackpot.groups.group1.length}${matchJackpot[0][0]} Jackpot`);
             }
         } else {
             console.log('invalid ticket');
@@ -20,4 +23,4 @@ function winningTicket(input) {
     }
 }
 
-winningTicket('$$$$$$$$$$$$$$$$$$$$f, aabb  , th@@@@@@eemo@@@@@@ey');
+winningTicket('$$$$$$$$$$$$$$$$$$$$, aabb  , th@@@@@@eemo@@@@@@ey');
