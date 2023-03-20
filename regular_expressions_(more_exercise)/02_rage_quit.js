cc6114b29bb13a41f7fdc76a0407cab87627cc60
Peter Zhechev <petar_vn7@abv.bg>
2023-03-20 17:11:14 +0200

    Update 02_rage_quit.js

--- regular_expressions_(more_exercise)/02_rage_quit.js
+++ regular_expressions_(more_exercise)/02_rage_quit.js
@@ -1,16 +1,28 @@
 function rageQuit(inputText) {
-    
-    let toUpperCaseInputText = inputText.toUpperCase();
 
-    let mainPattern = /[^0-9]{1,20}[0-9]{1,2}/g;
-
-    let mainMatched = toUpperCaseInputText.matchAll(mainPattern);
+    // let mainPattern = /(?<currentMessage>[^0-9]{1,20})(?<currentCount>[0-9]{1,2})/g;
+    let mainPattern = /(?<currentMessage>\D+)(?<currentCount>[0-9]+)/g;
+    let mainMatched = inputText[0].matchAll(mainPattern);
+    let outputMessage = '';
+    let uniqueSymbols = new Set();
 
     for (let el of mainMatched) {
-        console.log(el);
-    }
-    console.log(mainMatched);
 
+        let currentMessage = el.groups.currentMessage;
+        let currentCount = Number(el.groups.currentCount);
+
+        for (let i = 0; i < currentCount; i++) {
+            outputMessage += currentMessage;
+            for (let char of currentMessage) {
+                uniqueSymbols.add(char.toUpperCase());
+            }
+        }
+
+    }
+
+    console.log(`Unique symbols used: ${uniqueSymbols.size}\n${outputMessage.toUpperCase()}`);
 }
 
-rageQuit('aSd2&5s@1');
+// rageQuit(['aSd2&5s@1']);
+
+rageQuit(['e-!btI17z=E:DMJ19U1Tvg VQ>11P\"qCmo.-0YHYu~o%/%b.}a[=d15fz^\"{0^/pg.Ft{W12\`aD<l&$W&)*yF1WLV9_GmTf(d0($!$\`e/{D\'xi]-~17 *%p\"%|N>zq@ %xBD18<Y(fHh\`@gu#Z#p\"Z<v13fI]\':\Iz.17*W:\mwV\`z-15g@hUYE{_$~}+X%*nytkW15']);
