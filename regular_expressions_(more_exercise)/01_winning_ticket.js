3c98fdb54230300d279a44f55e285175bf6e9335
Peter Zhechev <petar_vn7@abv.bg>
2023-03-20 16:02:20 +0200

    Update 01_winning_ticket.js

--- regular_expressions_(more_exercise)/01_winning_ticket.js
+++ regular_expressions_(more_exercise)/01_winning_ticket.js
@@ -1,26 +1,51 @@
 function winningTicket(input) {
 
-    let winningTicketLength = 20;
+    let winningTicketLength = 10;
 
-    let pattern6to9 = /.+([@#$^]{6,}).+([@#$^]{6,}).+/;
-    // let jackpotPattern = /^(?<group1>([@#$^])\2{9})(?<group2>([@#$^])\4{9})$/;
-    let jackpotPattern = /^(?<group1>([@#$^])\2{9})(?<group2>\2{10})$/;
+    let pattern = /([@#$^])\1{5,}/;
 
     let splitted = input.split(', ');
-    let matchJackpot = [];
 
-    for (let el of splitted) {
+    for (let ticket of splitted) {
+        ticket = ticket.trim();
+        if (ticket.length === 20) {
 
-        matchJackpot = el.match(jackpotPattern);
+            ticket = ticket.split('');
+            let leftHalf = ticket.slice(0, 10).join('');
+            let rightHalf = ticket.slice(10, 20).join('');
+            ticket = ticket.join('');
 
-        if (matchJackpot) {
-            if (matchJackpot[0].length === 20) {
-                console.log(`ticket "${matchJackpot[0]}" - ${matchJackpot.groups.group1.length}${matchJackpot[0][0]} Jackpot`);
+            let leftMatched = leftHalf.match(pattern);
+            let rigthMatched = rightHalf.match(pattern);
+
+            if (leftMatched && rigthMatched) {
+
+                if (leftMatched[0].length === winningTicketLength && rigthMatched[0].length === winningTicketLength) {
+                    console.log(`ticket "${ticket}" - ${winningTicketLength}${leftMatched[1]} Jackpot!`);
+                } else {
+
+                    let currentWinningTicket = '';
+                    if (leftMatched[0].length > rigthMatched[0].length) {
+                        currentWinningTicket = rigthMatched[0];
+                    } else {
+                        currentWinningTicket = leftMatched[0];
+                    }
+                    console.log(`ticket "${ticket}" - ${currentWinningTicket.length}${currentWinningTicket[0]}`);
+                }
+
+            } else {
+                console.log(`ticket "${ticket}" - no match`);
             }
+
         } else {
             console.log('invalid ticket');
         }
     }
 }
 
-winningTicket('$$$$$$$$$$$$$$$$$$$$, aabb  , th@@@@@@eemo@@@@@@ey');
+// winningTicket('$$$$$$$$$$$$$$$$$$$$, aabb  , th@@@@@@eemo@@@@@@ey');
+
+// winningTicket('Cash$$$$$$Ca$$$$$$sh');
+
+// winningTicket('validticketnomatch:(');
+
