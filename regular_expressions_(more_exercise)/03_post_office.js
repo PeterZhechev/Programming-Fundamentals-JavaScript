3a2000e51621377382276c0ab6aff64521a6c996
Peter Zhechev <petar_vn7@abv.bg>
2023-03-22 17:18:26 +0200

    Update 03_post_office.js

--- regular_expressions_(more_exercise)/03_post_office.js
+++ regular_expressions_(more_exercise)/03_post_office.js
@@ -3,91 +3,45 @@ function postOffice(inputText) {
     [firstPart, secondPart, thirdPart] = inputText[0].split('|');
 
     let firstPattern = /(\#|\$|\%|\&|\*)(?<capitalLetters>[A-Z]+)\1/g;
-    let secondPattern = /(?<asciiCode>[0-9]{2}):(?<length>[0-9]{2})/g;
-    let thirdPattern = /(?<firstLetter>[A-Z])(?<remainingLetters>[^ ]+)/g;
+    let secondPattern = /(?<asciiCode>[0-9][0-9]):(?<length>01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20)/g;
 
     let firstMatch = [...firstPart.matchAll(firstPattern)];
     let secondMatch = [...secondPart.matchAll(secondPattern)];
-    let thirdMatch = [...thirdPart.matchAll(thirdPattern)];
+    let thirdMatch = thirdPart.split(' ');
 
-    let asciiCodeAndLengthPairs = [];
+    let capitalLettersAndTheirLength = new Map();
 
-    let capitalLetters = new Set();
-
-    for (let el of firstMatch) {
-        let currentCapitalLetters = el.groups.capitalLetters.split('');
-        for (let ch of currentCapitalLetters) {
-            capitalLetters.add(ch);
-        }
+    for (let currentLennter of firstMatch[0].groups.capitalLetters) {
+        capitalLettersAndTheirLength.set(currentLennter, 0);
     }
-
-    let unicalPairs = new Set();
 
     for (let pair of secondMatch) {
-        unicalPairs.add(pair[0]);
-    }
+        let asciiCodeToCapitalLetter = String.fromCharCode(pair.groups.asciiCode);
+        let length = pair.groups.length;
 
-    for (let pair of unicalPairs) {
-
-        let [asciiCode, length] = pair.split(':');
-        length = Number(length);
-
-        if (asciiCode >= 65 && asciiCode <= 90) {
-            let currentLetter = String.fromCharCode(asciiCode);
-            let currentPair = {};
-            currentPair[currentLetter] = length;
-            asciiCodeAndLengthPairs.push(currentPair);
-        }
-    }
-
-    let chechedLetters = [];
-
-    for (let ch of capitalLetters) {
-
-        for (let kvp of asciiCodeAndLengthPairs) {
-            for (let key in kvp) {
-                if (ch === key) {
-                    let currentObj = {};
-                    currentObj[ch] = kvp[key] + 1;
-                    chechedLetters.push(currentObj);
-                    break;
-                }
+        if (capitalLettersAndTheirLength.has(asciiCodeToCapitalLetter)) {
+            let currentLength = capitalLettersAndTheirLength.get(asciiCodeToCapitalLetter);
+            if (currentLength === 0) {
+                capitalLettersAndTheirLength.set(asciiCodeToCapitalLetter, Number(length));
             }
         }
     }
 
-    let list = new Set();
+    for (let letterAndLength of capitalLettersAndTheirLength) {
+        let currentValidLetter = letterAndLength[0];
+        let currentValidWordLength = letterAndLength[1];
 
-    for (let el of thirdMatch) {
-        let firstLetter = el.groups.firstLetter;
-        if (firstLetter.charCodeAt() >= 65 && firstLetter.charCodeAt() <= 90) {
-            list.add(el[0]);
-        }
-    }
-
-    let output = [];
-
-    for (let el of chechedLetters) {
-        let currentEntries = Object.entries(el);
-        let currentLetter = currentEntries[0][0];
-        let currentLength = Number(currentEntries[0][1]);
-        for (let el2 of list) {
-            let currentFirstListLetter = el2[0];
-            let currentListLength = el2.length;
-
-            if (currentLetter === currentFirstListLetter) {
-                if (currentLength === currentListLength) {
-                    output.push(el2);
+        for (let word of thirdMatch) {
+            if (currentValidLetter === word[0]) {
+                if (currentValidWordLength === word.length - 1) {
+                    console.log(word);
                 }
             }
         }
     }
-
-    console.log(output.join('\n'));
 }
 
 postOffice(['sdsGGasAOTPWEEEdas$AOTP$|a65:1.2s65:03d79:01ds84:02! -80:07++ABs90:1.1|adsaArmyd Gara So La Arm Armyw21 Argo O daOfa Or Ti Sar saTheww The Parahaos']);
 
 // postOffice(['Urgent"Message.TO$#POAML#|readData79:05:79:0!2reme80:03--23:11{79:05}tak{65:11ar}!77:!23--)77:05ACCSS76:05ad|Remedy Por Ostream :Istream Post sOffices Office Of Ankh-Morpork MR.LIPWIG Mister Lipwig']);
-
 
