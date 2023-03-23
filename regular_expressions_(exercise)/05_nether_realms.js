c522a39a2b77acf8178a22cbafde5804e8320c0a
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-03-23 17:29:04 +0200

    Update 05_nether_realms.js

--- regular_expressions_(exercise)/05_nether_realms.js
+++ regular_expressions_(exercise)/05_nether_realms.js
@@ -71,7 +71,7 @@ function netherRealms(input) {
 
 // netherRealms('M3ph-0.5s-0.5t0.0**');
 
-netherRealms('M3ph 1st0**, Azazel');
+netherRealms('M3ph1st0**, Azazel');
 
 // netherRealms('Gos/ho');
 
